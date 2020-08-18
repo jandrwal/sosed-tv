b88580ee463bf7c72e15acafda5ff8ec358bd2af
jandrwal <69856570+jandrwal@users.noreply.github.com>
2020-08-18 17:00:18 +0300

    Create sosed-tv.js

---
+++ sosed-tv.js
@@ -0,0 +1,76 @@
+var banned = {}, bannedNames
+
+window.setInterval
+(
+	function ()
+	{
+		Array.prototype.slice.call (document.getElementsByClassName ('chat__message')).forEach
+		(
+			function (i)
+			{
+				i.onclick = function (e)
+				{
+					if (e.ctrlKey)
+					{
+						banned [this.getAttribute ('data-from')] = this.firstElementChild.getAttribute ('data-name')
+						
+						bannedNames = Object.values (banned).filter (i => i).sort ((a, b) => a.localeCompare (b))
+						
+						console.log (bannedNames.join (', ') + ' попущен' + (bannedNames.length > 1	? 'ы' : ''))
+					}
+					
+					if (e.altKey)
+					{
+						var dataFrom = this.getAttribute ('data-from'), site, account, url
+						
+						if (dataFrom && dataFrom.split (':').length == 2)
+						{
+							site = dataFrom.split (':') [0]
+							account = dataFrom.split (':') [1]
+							
+							if (site == 'vkontakte')
+								url = 'https://vk.com/id'
+							else if (site == 'facebook')
+								url = 'https://www.facebook.com/profile.php?id='
+							else if (site == 'google')
+								url = 'https://www.google.com/maps/contrib/'
+								
+							url += account
+						}
+						
+						window.open (url)
+					}
+				}
+			}
+		)
+		
+		Object.keys (banned).forEach
+		(
+			function (i)
+			{
+				Array.prototype.slice.call (document.querySelectorAll ('[data-from="' + i + '"]')).forEach (i => i.remove ())
+			}
+		)
+	}, 
+	500
+)
+
+window.addEventListener
+(
+	'keydown', 
+	function (e)
+	{
+		if (e.altKey && e.keyCode == 67)
+		{
+			['header', 'main-video', 'tabs', 'rooms', 'users', 'popups hidden'].forEach
+			(
+				i => document.getElementsByClassName (i).length && document.getElementsByClassName (i) [0].remove ()
+			)
+			
+			document.getElementsByName ('ym-native-frame').length && document.getElementsByName ('ym-native-frame') [0].remove ()
+			
+			document.getElementsByClassName ('tabs__content').length && (document.getElementsByClassName ('tabs__content') [0].style.width = '600px')
+		}
+	}, 
+	false
+)
